a710c14cb5451580b84b4e4fa1ac9259c27b0f13
Federico Parma <federico.parma@unibs.it>
2024-12-11 15:25:23 +0100

    First commit

---
+++ include/openmore/trajectories_processors/thor_trajectory_processor.h
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <openmore/trajectories_processors/spline_trajectory_processor.h>
+#include  <thor_math/thor_math.h>
+
+/**
+ * @file thor_trajectory_processor.h
+ * @brief Contains the declaration of the ThorTrajectoryProcessor class.
+ */
+
+namespace openmore
+{
+
+struct QpIntervals
+/**
+ * @brief A Structurerepresenting the receiding horizon parameters.
+ */
+{
+public:
+  int nax; //number of DoF
+  int nc;  //number of control intervals (equal to number of predictions)
+  double control_horizon; 
+  double st;
+};
+typedef std::shared_ptr<QpIntervals> QpIntervalsPtr;
+
+struct QpWeigth
+/** 
+ * @brief A Structurwe representing the weigth of the cost function.
+ */
+{
+public:
+   
+  double lambda_acc;
+  double lambda_tau;
+  double lambda_scaling;
+  double lambda_clik;
+  double lambda_jerk;
+};
+typedef std::shared_ptr<QpWeigth> QpWeigthPtr;
+
+
+class ThorTrajectoryProcessor;
+typedef std::shared_ptr<ThorTrajectoryProcessor> ThorTrajectoryProcessorPtr;
+
+/**
+ * @brief The ThorTrajectoryProcessor class processes trajectories using spline interpolation and MPC
+*/
+
+class ThorTrajectoryProcessor: public SplineTrajectoryProcessor
+{
+
+public:
+  double max_t_;  
+
+
+protected:
+  /**
+   * @brief The Thor_math object for solving the QP problem.
+   */
+  thor::math::ThorQP thor;
+  double dt_ = 1e-3;
+  openmore::QpWeigthPtr weigths_;
+  openmore::QpIntervalsPtr intervals_;
+
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+
+
+  /**
+   * @brief Constructors.
+   * TODO chiamata costruttore di thor, assegnazione dei constraints a thor, costruttore che riceve in ingresso anche le funzioni dei pesi (definire una struct?)
+   * TODO INIT FUNCTION devono chiamare la funzione thor.setconstraints, fare nuova init function che riceve in ingresso anche i pesi e chiama thor.setweightfunction
+   * TODO funzione per settare il calcolo dei torque e dei position bounds, funzione per settare lo stato iniziale (può ricevere in ingresso un robotState, devo decomporlo)
+   * TODO ricordarsi di chiamare la funzione UpdateMatrices dopo aver settato tutte le cose di thor
+   * TODO da valutare: come gestiamo il dt (tempo tra due interpolazioni), se sempre fisso conviene definire all'inizio (quindi inserire nelle funzioni di init, a questo punto
+   * assieme anche a nc, control horizon e nax, così da chiamare assieme le fuinzioni setIntervals e updateMatrices). Se variabile invece bisogna definire una funzione che setta 
+   * il dt e chiamare updateMatrices nell'interpolzazione.
+   *
+   * 
+   */
+  ThorTrajectoryProcessor(const KinodynamicConstraintsPtr& constraints,
+                            const std::string& param_ns,
+                            const cnr_logger::TraceLoggerPtr& logger):
+    SplineTrajectoryProcessor(constraints,param_ns,logger){}
+
+  ThorTrajectoryProcessor(const KinodynamicConstraintsPtr& constraints,
+                            const std::string& param_ns,
+                            const cnr_logger::TraceLoggerPtr& logger,
+                            const std::vector<Eigen::VectorXd>& path):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,path){}
+
+  ThorTrajectoryProcessor(const KinodynamicConstraintsPtr& constraints,
+                            const std::string& param_ns,
+                            const cnr_logger::TraceLoggerPtr& logger,
+                            const spline_order_t& spline_order):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,spline_order){}
+
+  ThorTrajectoryProcessor(const KinodynamicConstraintsPtr& constraints,
+                            const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger,
+                            const std::vector<Eigen::VectorXd>& path,
+                            const spline_order_t& spline_order):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,path,spline_order){}
+
+  /**
+   * @brief init Initializes the TrajectoryProcessor object. This function should be called when the void constructor is called and it is used mainly for plugins.
+   * @param constraints The kinodynamics constraints of the robot to be considered for trajectory generation.
+   * @param param_ns_ The namespace under which to read the parameters with cnr_param.
+   * @param logger The logger for logging purposes.
+   * @param path The path for which the time-law need to be computed.
+   * @return
+   */
+  // virtual bool init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger) override;
+  // virtual bool init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path) override;
+
+  virtual bool init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const QpWeigthPtr weigths, const QpIntervalsPtr intervals);
+  virtual bool init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path,  const QpWeigthPtr weigths, const QpIntervalsPtr intervals);
+
+
+
+  /**
+   * @brief Function to compute the trajectory.
+   * @param initial_state The initial robot state.
+   * @param final_state The final robot state.
+   * @return True if the trajectory computation is successful, false otherwise.
+   */
+  //  virtual bool computeTrj() override;
+  //  virtual bool computeTrj(const RobotState& initial_state) override;
+  //  virtual bool computeTrj(const RobotState& initial_state, const RobotState& final_state) override;
+
+
+  /**
+   * @brief Interpolates a trajectory point at a given time.
+   * @param time The time at which to interpolate.
+   * @param pnt The interpolated trajectory point.
+   * @param scaling Scaling factor for interpolation.
+   * @return True if the interpolation is successful, false otherwise.
+   */
+  virtual bool interpolate(const double& time, TrjPointPtr& pnt, const double& target_scaling, double& updated_scaling) override;
+ 
+
+/**
+ * @brief Sets the weights for the trajectory processor.
+ * 
+ * This function assigns the provided weights to the trajectory processor, 
+ * which will be used in the optimization process.
+
+ * @param weigths A shared pointer to the QpWeigth object containing the weights.
+ */
+ virtual void  setWeigths(const QpWeigthPtr weigths);
+
+/**
+ * @brief Sets the intervals for the trajectory processor.
+ * 
+ * This function assigns the provided intervals to the trajectory processor.
+ * 
+ * @param intervals A shared pointer to the QpIntervals object containing the intervals to be set.
+ */
+ virtual void setIntervals(const QpIntervalsPtr intervals);
+
+ virtual void setConstraints();
+
+virtual bool computeTrj(std::string path_in);
+
+  bool computeTrj(){
+      return false;
+  }
+  
+  bool computeTrj(const RobotStatePtr& initial_state){
+      return false;
+  }
+  bool computeTrj(const RobotStatePtr& initial_state, const RobotStatePtr& final_state){
+      return false;
+  }
+
+virtual void setInitialState(const openmore::RobotStatePtr& initial_state);
+ };
+
+
+}
---
+++ src/thor_trajectory_processor.cpp
@@ -0,0 +1,188 @@
+#include <openmore/trajectories_processors/thor_trajectory_processor.h>
+#include  <thor_math/thor_math.h>
+
+
+
+namespace openmore
+{
+
+bool ThorTrajectoryProcessor::interpolate(const double& time, TrjPointPtr& pnt, const double& target_scaling, double& updated_scaling)
+{
+    //TODO da valutare: Posso mettere  questi dati nelle funzioni di inizializzazione/costruttori? nel caso servirebbe un dt sempre fisso. è così?
+    //TODO In caso prevedere funzione che mette in init nAx, nc e control_horizon, prevedere funzione che di soluzione in soluzione setta il dt.
+    
+    int nc = intervals_->nc;
+    int n_ax = intervals_->nax;
+
+    // Inizialization of the reference vectors
+    Eigen::VectorXd target_Dq(nc*n_ax);
+    target_Dq.setOnes();
+    
+    Eigen::VectorXd next_Q(n_ax);
+    next_Q.setZero(); 
+    
+    // Interpolate with the spline class for each prediction time instant
+    Eigen::VectorXd istants =  thor.getPredictionTimeInstant();
+    for (int i = 0; i < istants.size(); i++){
+        if ( !openmore::SplineTrajectoryProcessor::interpolate(time+istants[i], pnt, target_scaling, updated_scaling) ){
+            return false;
+        }
+        if (i==0){
+            // Assign values to traget vectors
+            next_Q.head(n_ax) = Eigen::VectorXd::Map(pnt->state_->pos_.data(), n_ax);
+
+            
+        }
+
+        target_Dq.segment(i*n_ax, n_ax) = Eigen::VectorXd::Map(pnt->state_->vel_.data(), n_ax);        
+    }
+    
+    Eigen::VectorXd next_acc(n_ax);
+    Eigen::VectorXd next_vel(n_ax);
+    Eigen::VectorXd next_pos(n_ax);
+
+    // Receiding horizon method
+    thor.computedCostrainedSolution(target_Dq,next_Q,target_scaling,thor.getState(),next_acc,updated_scaling);
+    
+    // Update of the solver state and of the solution point
+    next_pos = thor.getState().head(n_ax);
+    next_vel = thor.getState().tail(n_ax);
+
+    thor.updateState(next_acc);
+    pnt->state_->acc_.assign(next_acc.data(), next_acc.data() + next_acc.size());
+    pnt->state_->vel_.assign(next_vel.data(), next_vel.data() + next_vel.size());
+    pnt->state_->pos_.assign(next_pos.data(), next_pos.data() + next_pos.size());
+    return true;
+}
+
+void ThorTrajectoryProcessor::setWeigths(const QpWeigthPtr weigths)
+{
+    double lambda_acc = weigths->lambda_acc;
+    double lambda_clik = weigths->lambda_clik;
+    double lambda_tau = weigths->lambda_tau;
+    double lambda_scaling = weigths->lambda_scaling;
+    double lambda_jerk = weigths->lambda_jerk;
+    weigths_ = weigths;
+    thor.setWeigthFunction(lambda_acc,lambda_tau,lambda_jerk,lambda_scaling,lambda_clik);
+
+
+}
+
+void ThorTrajectoryProcessor::setIntervals(const QpIntervalsPtr intervals)
+{
+    int nax= intervals->nax;
+    int nc=intervals->nc;
+    double control_horizon=intervals->control_horizon;
+    double st=intervals->st;
+    intervals_ = intervals;
+    thor.setIntervals(nc,nax,control_horizon,st);    
+
+}
+
+void ThorTrajectoryProcessor::setConstraints(){
+    Eigen::VectorXd qmax = kinodynamic_constraints_->max_pos_;
+    Eigen::VectorXd Dqmax = kinodynamic_constraints_->max_vel_;
+    Eigen::VectorXd DDqmax = kinodynamic_constraints_->max_acc_;
+    Eigen::VectorXd tau_max = kinodynamic_constraints_->max_eff_;
+
+    Eigen::VectorXd qmin = kinodynamic_constraints_->min_pos_;
+    Eigen::VectorXd Dqmin = kinodynamic_constraints_->min_vel_;
+    Eigen::VectorXd DDqmin = kinodynamic_constraints_->min_acc_;
+    Eigen::VectorXd tau_min = kinodynamic_constraints_->min_eff_;
+
+    thor.setConstraints(qmax,qmin,Dqmax,DDqmax,tau_max);
+
+}
+
+
+bool ThorTrajectoryProcessor::init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const QpWeigthPtr weigths, const QpIntervalsPtr intervals){
+
+    openmore::SplineTrajectoryProcessor::init(constraints, param_ns, logger);
+    ThorTrajectoryProcessor::setWeigths(weigths);
+    ThorTrajectoryProcessor::setIntervals(intervals);
+    ThorTrajectoryProcessor::setConstraints();
+
+    thor.activateTorqueBounds(false);
+    if (thor.needUpdate())
+    { 
+    thor.updateMatrices();
+    }
+
+
+    return true;
+
+}
+
+bool ThorTrajectoryProcessor::init(const KinodynamicConstraintsPtr& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path,  const QpWeigthPtr weigths, const QpIntervalsPtr intervals)
+{
+    openmore::SplineTrajectoryProcessor::init(constraints, param_ns, logger, path);
+    ThorTrajectoryProcessor::setWeigths(weigths);
+    ThorTrajectoryProcessor::setIntervals(intervals);
+    ThorTrajectoryProcessor::setConstraints();
+    return true;
+}
+bool ThorTrajectoryProcessor::computeTrj(std::string path_in){
+
+    if (!intervals_) {
+        throw std::runtime_error("Intervals are not initialized.");
+    }
+    int nax = intervals_->nax;
+    
+
+
+    std::string config_path = path_in;
+    YAML::Node yaml_path_ns;
+    try {
+        yaml_path_ns = YAML::LoadFile(config_path);
+    } catch (const YAML::BadFile& e) {
+        std::cerr << "Error loading file: " << config_path << std::endl;
+        return false;
+    }
+    std::vector<double> time = yaml_path_ns["time"].as<std::vector<double>>();
+    std::vector<std::vector<double>> pos_vec;
+    std::vector<std::vector<double>> vel_vec;
+
+
+    
+    for (const auto& node : yaml_path_ns["positions"]) {
+        std::vector<double> vec = node.as<std::vector<double>>();
+        pos_vec.push_back(vec);
+        
+    }
+    
+    for (const auto& node : yaml_path_ns["velocities"]) {
+        std::vector<double> vec = node.as<std::vector<double>>();
+       vel_vec.push_back(vec);
+    }
+   
+    
+    for (int i =0; i< vel_vec.size(); i++){
+        openmore::TrjPointPtr point = std::make_shared<openmore::TrjPoint>();
+        point->state_ = std::make_shared<openmore::RobotState>();
+        point->state_->pos_.resize(nax);
+        point->state_->vel_.resize(nax);
+        point->state_->acc_.resize(nax);
+        point->state_->eff_.resize(nax);
+
+        point->time_from_start_ = time[i];
+        point->state_->pos_ = pos_vec[i];
+        point->state_->vel_ = vel_vec[i]; 
+
+        trj_.push_back(point);
+        max_t_=time[i];
+    }
+        
+      
+    return true;
+
+    }
+
+    void ThorTrajectoryProcessor::setInitialState(const openmore::RobotStatePtr& initial_state){
+        int nax = intervals_->nax;
+        Eigen::VectorXd state(2*nax);
+        state.head(nax) = Eigen::VectorXd::Map(initial_state->pos_.data(), nax);
+        state.tail(nax) = Eigen::VectorXd::Map(initial_state->vel_.data(), nax);
+        thor.setInitialState(state);
+    }
+
+}
